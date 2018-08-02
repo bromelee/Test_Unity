68792c06651342bb05fb9cf5da916ede57a9c836
bromelee <bromelee@yahoo.es>
2018-08-02 18:03:59 -0500

    first comit

---
+++ Assets/my work/regina/changepose.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class changepose : MonoBehaviour {
+
+	int pose;
+	Animator myani;
+	float cont = 5f;
+
+	// Use this for initialization
+	void Start () {
+		pose = 0;
+		myani = this.GetComponent<Animator> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (cont > 0) {
+			cont -= Time.deltaTime;
+		} else {
+			pose = Random.Range (0, 4);
+            myani.SetInteger("position", pose);
+			cont = 5f;
+		}
+	}
+}
---
+++ Assets/my work/ybot/ybotcontrol.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ybotcontrol : MonoBehaviour {
+
+    int pose;
+    Animator myani;
+    float cont = 15f;
+
+    // Use this for initialization
+    void Start()
+    {
+        pose = 0;
+        myani = this.GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (cont > 0)
+        {
+            cont -= Time.deltaTime;
+        }
+        else
+        {
+            pose = Random.Range(1, 5);
+            myani.SetInteger("anims", pose);
+            cont = 15f;
+        }
+    }
+
+}
